9e7f648df3e90d1da086a59ef86b872ad67a13f1
Mukund Srinivasan <mukundsrinivasan0@gmail.com>
2026-02-10 15:27:31 +1100

    added environment_mapping

---
+++ environment_mapping/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'environment_mapping'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mksneo',
+    maintainer_email='mukundsrinivasan0@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'environment_mapping_node = environment_mapping.environment_mapping_node:main'
+        ],
+    },
+)
